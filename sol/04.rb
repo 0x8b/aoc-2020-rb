83c9ec8cf769286c149070a815cfb7e9af603f2f
Mariusz Gumienny <mkgumienny@gmail.com>
2020-12-04 12:54:31 +0100

    4

--- sol/04.rb
+++ sol/04.rb
@@ -4,7 +4,7 @@ end.map do |passport|
   passport.join(" ").split(" ").map do |field|
     field.split ":"
   end.to_h
-end.to_a
+end
 
 required_fields = %w(byr iyr eyr hgt hcl ecl pid)
 
@@ -15,14 +15,14 @@ end
 puts valid.count # 250
 
 strict_valid = valid.select do |p|
-  t1 = p["byr"].to_i.between? 1920, 2002
-  t2 = p["iyr"].to_i.between? 2010, 2020
-  t3 = p["eyr"].to_i.between? 2020, 2030
+  t1 = (1920..2002) === p["byr"].to_i
+  t2 = (2010..2020) === p["iyr"].to_i
+  t3 = (2020..2030) === p["eyr"].to_i
   h  = p["hgt"]
-  t4 = h =~ /cm$/ && h.to_i.between?(150, 193)
-  t5 = h =~ /in$/ && h.to_i.between?(59, 76)
-  t6 = p["hcl"] =~ /^#[a-f0-9]{6}$/
-  t7 = p["pid"] =~ /^[0-9]{9}$/
+  t4 = h =~ /cm$/ && (150..193) === h.to_i
+  t5 = h =~ /in$/ && (59..76) === h.to_i
+  t6 = p["hcl"] =~ /^#[a-f\d]{6}$/
+  t7 = p["pid"] =~ /^\d{9}$/
   t8 = "amb blu brn gry grn hzl oth".include? p["ecl"]
 
   [t1, t2, t3, (t4 || t5), t6, t7, t8].all?
