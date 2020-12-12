13aab6fb7badb52ed2ee1cba9f36fcdf93ec8ffd
Mariusz Gumienny <mkgumienny@gmail.com>
2020-12-12 20:17:00 +0100

    12

--- sol/12-golf.rb
+++ sol/12-golf.rb
@@ -1,5 +1,5 @@
-k,n,s,e,w,r,l=0,1i,-1i,1,-1,-1i,1i;a,d,b,x,u=k,e,k,10+n,->y{a+=y;x+=y}
-DATA.map{|q|t,v=q[0],q[1..].to_i;z=v/90
-case t;in ?N;u[v*n];in ?S;u[v*s];in ?W;u[v*w];in ?E;u[v*e]
-in ?F;a+=v*d;b+=v*x;in ?L;d*=l**z;x*=l**z;in ?R;d*=r**z;x*=r**z;end}
-[a,b].map{|w|p w.rect.sum &:abs}
+a,d,e,n=0,1i,1,-1;k,w,b,x,u,l,v,t=a,e,a,10+d,->y{k+=y;x+=y},d,-d,-d
+DATA.map{|q|r,s=q[0],q[1..].to_i;z=s/90
+case r;in ?N;u[s*d];in ?S;u[s*v];in ?W;u[s*n];in ?E;u[s*e]
+in ?F;k+=s*w;b+=s*x;in ?L;w*=l**z;x*=l**z;in ?R;w*=t**z;x*=t**z;end}
+[k,b].map{|n|p n.rect.sum &:abs}
