c3bc0c3566554b5084d409d26f5c751b50e61cd2
Mariusz Gumienny <mkgumienny@gmail.com>
2020-12-17 10:51:11 +0100

    17

--- sol/17.rb
+++ sol/17.rb
@@ -1,73 +1,66 @@
 require 'set'
 
-initial_layer = DATA.lines.map do |line| line.strip.split("").map { |e| e == ?# ? 1 : 0} end
-
-cube = Hash.new { |h, k| h[k] = 0 }
-
-initial_layer.size.times do |y|
-  initial_layer[0].size.times do |x|
-    cube[[x, y, 0]] = initial_layer[y][x]
+initial_layer = DATA.each_line.map do |line|
+  line.strip.chars.map do |c|
+    c == ?# ? :active : :inactive
   end
 end
 
-def print_cube cube, label
-  xs, ys, zs = cube.keys.transpose
-  min_x, max_x = xs.minmax
-  min_y, max_y = ys.minmax
-  min_z, max_z = zs.minmax
+cube = Hash.new do |h, k|
+  if k.size == 3
+    h[k] = :inactive
+  end
+end
 
-  puts "\n#{label}:\n"
-  for z in (min_z..max_z)
-    puts "\nz=#{z}"
-    for y in (min_y..max_y)
-      for x in (min_x..max_x)
-        print cube[[x, y, z]] == 1 ? ?# : ?.
+tesseract = Hash.new do |h, k|
+  if k.size == 4
+    h[k] = :inactive
+  end
+end
+
+initial_layer[0].size.times do |x|
+  initial_layer.size.times do |y|
+    cube[[x, y, 0]] = initial_layer[x][y]
+    tesseract[[x, y, 0, 0]] = initial_layer[x][y]
+  end
+end
+
+def run space, dimensions
+  1.upto(6) do |i|
+    new_space = Hash.new { |h, k| h[k] = :inactive }
+
+    extended_keys = space.keys.flat_map do |point|
+      [-1, 0, 1].repeated_permutation(dimensions).map do |deltas|
+        point.zip(deltas).map &:sum
       end
-      print "\n"
+    end.to_set
+
+    extended_keys.each do |point|
+      value = space[point]
+
+      neighbours = ([-1, 0, 1].repeated_permutation(dimensions).to_a - [[0] * dimensions]).map do |deltas|
+        point.zip(deltas).map &:sum
+      end
+
+      if space[point] == :active && ((2..3) === neighbours.count { |pt| space[pt] == :active })
+        new_space[point] = :active
+      else
+        new_space[point] = :inactive
+      end
+
+      if space[point] == :inactive && (neighbours.count { |pt| space[pt] == :active } == 3)
+        new_space[point] = :active
+      end
     end
+
+    space = new_space
+
+    puts "Number of active after #{i} cycle#{i == 1 ? '' : ?s}: #{space.values.count :active }"
   end
-  puts ""
 end
 
-print_cube cube, "Before any cycles"
-
-1.upto(6) do |i|
-  new_cube = Hash.new { |h, k| cube[k] = 0 }
-
-  extended_keys = cube.keys.flat_map do |x, y, z|
-    [-1, 0, 1].repeated_permutation(3).map do |dx, dy, dz|
-      [x + dx, y + dy, z + dz]
-    end
-  end.to_set
-
-  extended_keys.each do |key|
-    value = cube[key]
-    x, y, z = key
-
-    neighbours = ([-1, 0, 1].repeated_permutation(3).to_a - [[0, 0, 0]]).map do |dx, dy, dz|
-      [x + dx, y + dy, z + dz]
-    end
-
-    # new_cube[key] = cube[key]
-    if cube[key] == 1 && ((2..3) === neighbours.count { |k| cube[k] == 1 })
-      new_cube[key] = 1
-    else
-      new_cube[key] = 0
-    end
-
-    if cube[key] == 0 && (neighbours.count { |k| cube[k] == 1} == 3)
-      new_cube[key] = 1
-    else
-      # new_cube[key] = 0
-    end
-  end
-
-  cube = new_cube
-
-  print_cube cube, "After #{i} cycle#{i == 1 ? '' : ?s}"
-
-  puts "Active  after #{i} cycle#{i == 1 ? '' : ?s}: #{cube.values.count(1)}" # 271
-end
+run cube, 3 # 271
+run tesseract, 4 # 2064
 
 __END__
 #......#
